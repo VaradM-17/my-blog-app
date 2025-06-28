8d4ecf57eaf12b448764fac61c9182a806c10a19
Varad Mule <varadmule17@gmail.com>
2025-06-28 17:54:48 +0530

    post entity added

---
+++ blog-app/src/main/java/com/spring/blog_app/entity/Post.java
@@ -0,0 +1,34 @@
+package com.spring.blog_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts", uniqueConstraints = { @UniqueConstraint(columnNames = { "title" }) })
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "descrition", nullable = false)
+	private String descrition;
+
+	@Column(name = "content", nullable = false)
+	private String content;
+}
